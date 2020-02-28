7d477f3a5f71663f6ecb5886af26afdba3786beb
Fraser Carpenter <fraser@hqtech.co.za>
2020-02-28 15:34:47 +0200

    user backend

--- CipApp.API/Startup.cs
+++ CipApp.API/Startup.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CipApp.API.ConcreteRepositories;
 using CipApp.API.Data;
+using CipApp.API.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +34,7 @@ namespace CipApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
